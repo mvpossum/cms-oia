f6f5c4f0f03195fee25d9cacb2b21eb9fb7d2dfe
Oia Poli <politecnico.oia@gmail.com>
2015-09-04 12:40:06 -0300

    new labels

--- util_scripts/genlabels.py
+++ util_scripts/genlabels.py
@@ -9,7 +9,7 @@ if len(argv)!=3:
     print ("Creates a pdf with the labels\nUsage: {exe} users.csv output.tex".format(exe=argv[0]))
     exit(1)
 
-with open (os.path.join(os.path.dirname(__file__), "template.tex"), "r") as tf:
+with open (os.path.join(os.path.dirname(__file__), "templatelabels.tex"), "r") as tf:
     raw=tf.read()
 [(head, tail)] = re.compile("(.*)%USERS_WILL_GO_HERE%(.*)", re.DOTALL).findall(raw)
 
@@ -17,7 +17,7 @@ with open(argv[2], "w") as out:
     out.write(head)
     reader=csv.DictReader(open(argv[1], "r"))
     for row in reader:
-        out.write("\\user{{{0} {1}}}{{{2}}}{{{3}}}\n".format(row['Nombre'],row['Apellido'], row['Username'], row['Password']))
+        out.write("\\user{{{0} {1}}}{{{2}}}{{{3}}}{{{4}}}\n\n".format(row['Nombre'],row['Apellido'], row['Username'][0], row['Username'], row['Password']))
     out.write(tail)
 
 call(["pdflatex", argv[2]])
